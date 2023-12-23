9d2c6df4f5563d4e7ce498fa9b29d00ddd0d0d27
Ken C <kenwillcode@outlook.com>
2023-12-23 15:08:32 -0500

    established skills images template draft and base display behavior

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,13 @@ import React, { useState } from 'react';
 import '../styles/Skills.css';
 
 const skillsData = [
-  { id: 1, label: 'HTML', imageUrl: 'html-logo.png', description: 'HTML is...' },
+  { id: 1, label: 'HTML', imageName: 'html-logo.png', description: 'HTML is...' },
+  { id: 2, label: 'CSS', imageName: 'css-logo.png', description: 'CSS is...' },
+  { id: 3, label: 'JavaScript', imageName: 'javascript-logo.png', description: 'JavaScript is...' },
+  { id: 4, label: 'NestJS', imageName: 'nest-logo-red.jpg', description: 'NestJS is...' },
+  { id: 5, label: 'ReactJS', imageName: 'react-logo.png', description: 'ReactJS is...' },
+
+
   // Add more skills data
 ];
 
@@ -21,14 +27,14 @@ const Skills = () => {
       <div className="skill-container">
         {skillsData.map((skill) => (
           <div key={skill.id} onClick={() => handleSkillClick(skill.id)} className="skill-item">
-            <img src={skill.imageUrl} alt={skill.label} />
+            <img src={`../resources/${skill.imageName}`} alt={skill.label} />
             <p>{skill.label}</p>
           </div>
         ))}
       </div>
       {selectedSkill && (
         <div className="selected-skill">
-          <img src={selectedSkill.imageUrl} alt={selectedSkill.label} />
+          <img src={`../resources/${selectedSkill.imageName}`} alt={selectedSkill.label} />
           <h3>{selectedSkill.label}</h3>
           <p>{selectedSkill.description}</p>
         </div>
